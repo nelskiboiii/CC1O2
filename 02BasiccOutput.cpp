4d4ab966a06d2fb364b91d4a1a7e2e530ed4c225
kn <nelsonkingubalde@gmail.com>
2024-10-03 12:37:01 +0800

    Update 02BasiccOutput.cpp

--- 02BasiccOutput.cpp
+++ 02BasiccOutput.cpp
@@ -3,11 +3,10 @@
 using namespace std;
 
 int main () {
-
-	cout << "Our Lady Of Lourdes College Founfation/n";
-	cout << "College of Computer Science/n";
-	cout << "BSIT - 1st Year/n";
-	cout << "King Nelson U. Llanto/n";
-	cout << "Alegre Street, Brgy. Poblacion II, San Vicente Camarines Norte/n";
+	cout << "Our Lady Of Lourdes College Founfation\n";
+	cout << "College of Computer Science\n";
+	cout << "BSIT - 1st Year\n";
+	cout << "King Nelson U. Llanto\n";
+	cout << "Alegre Street, Brgy. Poblacion II, San Vicente Camarines Norte\n";
 	return 0; 
 }
