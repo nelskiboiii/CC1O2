287600ba6cfc7e24af1912413e9da6b8378faf27
kn <nelsonkingubalde@gmail.com>
2024-10-03 09:43:57 +0800

    Add files via upload

---
+++ 03VariableeDeclaration.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int age;
+	string name;
+	
+	cout << "Enter you age: ";
+	cin >> age;
+	cout << "you are " << age << "years old" << endl;
+	return 0;
+	
+}
