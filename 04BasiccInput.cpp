794e425f0f18bf1a8d2401519d0d6256d897ac05
kn <nelsonkingubalde@gmail.com>
2024-10-03 10:02:05 +0800

    Add files via upload

---
+++ 04BasiccInput.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	string Nickname, cyl, School;
+	
+	cout << "Enter you nickname: ";
+	cin >> Nickname;
+	cout << "Your Nickname is " << Nickname << endl;
+	
+	cout << "Enter your Course-Year Level:";
+	cin >> cyl;
+	cout << "Your Course-Year Level is " << cyl << endl;
+	
+	cout << "Enter your Scool:";
+	cin >> School;
+	cout << "Your School is " << School << endl;
+	
+	return 0;
+}
