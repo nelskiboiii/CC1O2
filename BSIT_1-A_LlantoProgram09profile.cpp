9eb3ed6c45ee1081149d8ecf84d36998d663eb86
kn <nelsonkingubalde@gmail.com>
2024-10-03 12:59:53 +0800

    Add files via upload

---
+++ BSIT_1-A_LlantoProgram09profile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	cout << "Name: King Nelson U. Llanto" << endl;
+	cout << "Course/YrLvl/section: BSIT1-A" << endl;
+	cout << "Address: Alegre Street San Vicente Camarines Norte" << endl;
+	cout << "*******************************************************************************" << endl;
+	cout << "YES! PROGRAMMER NA KO GUYS <3 uwu!" <<endl;
+	cout << "*******************************************************************************" << endl;
+	cout << "Favorite: black" <<endl;
+	return 0;
+	
+}
