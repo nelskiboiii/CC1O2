9ebe28c1c0cbcbda86aaf587c5a830d45680f779
kn <nelsonkingubalde@gmail.com>
2024-10-03 12:05:49 +0800

    Update 03VariableeDeclaration.cpp

--- 03VariableeDeclaration.cpp
+++ 03VariableeDeclaration.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
+int main () {
 	int age;
-	string name;
 	
 	cout << "Enter you age: ";
 	cin >> age;
-	cout << "you are " << age << "years old" << endl;
+	cout << "you are " << age << "years old";
 	return 0;
-	
 }
