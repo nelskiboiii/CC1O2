3ebe71c3737929afc360f4ddc7b80f87d8a2a267
kn <nelsonkingubalde@gmail.com>
2024-10-03 09:28:47 +0800

    Add files via upload

---
+++ 02BasiccOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+	cout << " Our Lady Of Lourdes College Founfation " <<endl;
+	cout << " College of Computer Science " <<endl;
+	cout << " BSIT - 1st Year "<<endl;
+	cout << " King Nelson U. Llanto " <<endl;
+	cout << " Alegre Street, Brgy. Poblacion II, San Vicente Camarines Norte " <<endl;
+	return 0;
+
+}
---
+++ O1Hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+	cout << "HOWDY LOURDENIAN BPROGRAMMERS!";
+	RETURN 0;
+
+}
