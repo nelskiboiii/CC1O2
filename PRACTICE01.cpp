ec7575c6e16214997b5bf64c85d810d618cfce21
kn <nelsonkingubalde@gmail.com>
2024-10-09 11:29:18 +0800

    Add files via upload

---
+++ PRACTICE01.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int a, b;
+	
+	//SUM
+	cout << "***************************************" << endl;
+	cout << "F I N D I N G    S U M" << endl;
+	cout << "Enter the number: ";
+	cin >> a;
+	cout << "Enter the number: ";
+	cin >> b;
+	cout << "Sum: " << a << '+' << b << '=' << a+b << endl;
+	cout << "***************************************" << endl;
+	
+	// Difference
+	cout << "***************************************" << endl;
+	cout << "F I N D I N G    D I F F E R E N C E" << endl;
+	cout << "Enter the number: ";
+	cin >> a;
+	cout << "Enter the number: ";
+	cin >> b;
+	cout << "Difference: " << a << '-' << b << '=' << a-b << endl;
+	cout << "***************************************" << endl;
+	
+	// Product
+	cout << "***************************************" << endl;
+	cout << "F I N D I N G   P R O D U C T" << endl;
+	cout << "Enter the number: ";
+	cin >> a;
+	cout << "Enter the number: ";
+	cin >> b;
+	cout << "Product: " << a << '*' << b << '=' << a*b << endl;
+	cout << "***************************************" << endl;
+	
+	// Quotient
+	cout << "***************************************" << endl;
+	cout << "F I N D I N G    Q O U T I E N T" << endl;
+	cout << "Enter the number: ";
+	cin >> a;
+	cout << "Enter the number: ";
+	cin >> b;
+	cout << "Quotient: " << a << '/' << b << '=' << a/b << endl;
+	cout << "***************************************" << endl;
+	
+	// Remainder
+	cout << "***************************************" << endl;
+	cout << "F I N D I N G    R E M A I N D E R" << endl;
+	cout << "Enter the number: ";
+	cin >> a;
+	cout << "Enter the number: ";
+	cin >> b;
+	cout << "Remainder: " << a << '%' << b << '=' << a%b << endl; 
+	cout << "***************************************" << endl;
+	return 0;
+}
+	
+	
