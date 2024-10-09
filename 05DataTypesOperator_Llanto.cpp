1452bbaa34b9bc7e02f95d6bde50aa5eabf5be1d
kn <nelsonkingubalde@gmail.com>
2024-10-09 09:51:30 +0800

    Update 05DataTypesOperator_Llanto.cpp

--- 05DataTypesOperator_Llanto.cpp
+++ 05DataTypesOperator_Llanto.cpp
@@ -4,26 +4,20 @@ using namespace std;
 
 int main() {
 
+	cout << "---------S I M P L E   C A L C U L A T O R---------" << endl;
 	int a, b;
-	
-	cout << "Enter First Number: ";
-	
+	cout << "---------------------------------------------------" << endl;	
+	cout << "Enter 1st Number: ";
 	cin >> a;
-	
-	cout << "Enter Second Number: ";
-	
+	cout << "---------------------------------------------------" << endl;	
+	cout << "Enter 2nd Number: ";
 	cin >> b;
-	
+	cout << "---------------------------------------------------" << endl;	
 	cout << "Sum: " << a << '+' << b << '=' << a+b << endl;
-	
 	cout << "Difference: " << a << '-' << b << '=' << a-b << endl;
-	
 	cout << "Product: " << a << '*' << b << '=' << a*b << endl;
-	
 	cout << "Quotient: " << a << '/' << b << '=' << a/b << endl;
-	
 	cout << "Remainder: " << a << '%' << b << '=' << a%b << endl;
-	
+	cout << "---------------------------------------------------" << endl;	
 	return 0;
-	
 }
