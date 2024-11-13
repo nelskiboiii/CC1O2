0ce88343b2355e080d07cfa9deb65d41a8776076
kn <nelsonkingubalde@gmail.com>
2024-11-13 10:04:08 +0800

    Update

---
+++ LLANTO_ACTIVITY2_AGE.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+  int response;
+  cout << "What is your age?:  ";
+  cin >> response;
+  if (age < 16) { 
+    cout << " You are too young to drive";
+}
+  else if ( age <= 16 ) {
+    cout <<  " Better clear the road";
+}
+  else if ( age >16) {
+    cout << "You are getting pretty old":
+}
+  return 0;
+}
